98d20239eb8556e91961f1ddd72f51554c4bda23
FlameAndFlash <h2wiens@uwaterloo.ca>
2026-03-07 16:06:31 -0500

    Downloads a step file

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from backend.open_step_file import open_step_file
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Backend running"}
+
+@app.get("/download-step-file")
+def download_step_file():
+    return open_step_file()
