0975fdd0075b80de9178d895fa4165ebe48df462
Elizabeth Wright <e3wright@uwaterloo.ca>
2026-03-07 13:26:59 -0500

    added vuetify & basic styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <v-app>
+    <v-locale-provider>
+      <v-app-bar scroll-behavior="hide" app>wooooo</v-app-bar>
+      <v-main id="main">
+        <router-view />
+      </v-main>
+    </v-locale-provider>
+  </v-app>
 </template>
 
 <style scoped></style>
