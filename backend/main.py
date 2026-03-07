0c30de6a5769e47097f7c973133cd7bf396c198e
FlameAndFlash <h2wiens@uwaterloo.ca>
2026-03-07 18:49:49 -0500

    Created upload function

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 from open_step_file import open_step_file
+from accept_png import accept_png
 
 app = FastAPI()
 
@@ -10,3 +12,16 @@ def root():
 @app.get("/download-step-file")
 def download_step_file():
     return open_step_file()
+
+@app.post("/upload")
+async def upload(image: UploadFile = File(...)):
+    result = await accept_png(image)
+    return result
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
